b70586931d427883271facd30d887deb691fab35
Samantha brake <sammy.brake@gmail.com>
2018-11-04 22:26:04 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT Characters.species, COUNT(Characters.species) FROM characters GROUP BY Characters.species ORDER BY COUNT(Characters.species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
